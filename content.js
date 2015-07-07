c0ae85bbf714e2c41df5b2813b05cf031fbea79e
Milan Medlik <milan.medlik@gmail.com>
2015-07-07 19:01:16 +1200

    create class components for babel

--- content.js
+++ content.js
@@ -1,1 +1,40 @@
-module.exports = 'Pozdrav z content.js'
+export class Auto {
+  constructor(znacka){
+    this._znacka = znacka;
+  }
+
+  toString(){
+  	return 'Auto::' + this._znacka;
+  }
+}
+
+export default class Zavodak extends Auto {
+  constructor(znacka, maximalka){
+    super(znacka);
+    this._upravy = 'Nove';
+    this._maximalka = maximalka;
+  }
+  
+  get maximalka(){
+      return this._maximalka;
+  }
+
+  set maximalka(maximalka){
+  	this._upravy = 'Upraveno';
+  	this._maximalka = maximalka;
+  }
+
+  setGumy(gumy){
+    this.gumy = gumy;
+  }
+
+  toString(){
+  	return super.toString() + ' Maximalka::' + this._maximalka + ' Stav:: ' + this._upravy;
+  }
+}
+
+export const GUMY = {
+  sucho: {},
+  mokro: {},
+  snih : {}
+}
--- entry.js
+++ entry.js
@@ -1,3 +1,11 @@
-require('./style.css')
-var pozdrav = require('./content.js')
-document.write(pozdrav);
+import './style.css';
+import Zavodak, {Gumy as GumyNa, Auto} from "./content.js";
+
+
+var Subaru = new Zavodak('Subaru', 250);
+document.write(Subaru.toString() + '<br/>');
+
+Subaru.maximalka = 280;
+document.write(Subaru.toString() + '<br/>');
+
+Sumbary.setGumy(GumyNa.sucho);
